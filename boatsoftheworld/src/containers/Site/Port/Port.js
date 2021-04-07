c60e281d6e6ae1d17691b84710412dec72c22315
Etienne NDOUR <etienndour@yahoo.fr>
2021-04-08 00:23:37 +0200

    Port, Accueil and Bateau files updated OK

--- boatsoftheworld/src/containers/Site/Port/Port.js
+++ boatsoftheworld/src/containers/Site/Port/Port.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import TitreH3 from '../../../components/UI/Titres/TitreH3';
+import axios from "axios";
 
 class Port extends Component {
+    state = {
+        bateaux : null,
+    }
+
+    componentDidMount = () => {
+       axios.get(`http://localhost/BOATSSERVER/front/bateaux`)
+         .then(response => {
+             this.setState({boats:Object.values(response.data)});
+         })
+    }
+
     render() {
         return (
             <>
                 <TitreH3 bgColor="bg-primary">Les embarcations</TitreH3>
                 <div className="container">
-                    Page listant les embarcations du port.
+                    {
+                       this.state.boats &&
+                       this.state.boats.map(boat => {
+                           return <h1>{boat.id} - {boat.name}</h1>
+                       }) 
+                    }
                 </div>
             </>
         );
